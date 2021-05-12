7d5872a8d5647f81d755ced394e7d18c1ce7f707
AmirAR-QA <aabdur-rahman@academytrainee.com>
2021-05-12 16:38:30 +0100

    finished tasks 1 to 3

--- Todo/app.py
+++ Todo/app.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__) 
 
-app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://root:root@35.242.146.124:3306/potato"
+app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://root:root@35.197.230.179/todolist"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False 
 db = SQLAlchemy(app) 
 
@@ -16,6 +16,34 @@ class Customers(db.Model):
 @app.route('/home')
 def home():
     return 'This is a to-do list application!'
+
+@app.route('/new')
+def _new():
+    todos1 = todos(Task="New To-do", Complete=True)
+    db.session.add(todos1)
+    db.session.commit()
+    return "You've added a new task to do"
+
+@app.route("/complete/<int:id>")
+def complete(id):
+    todos1 = todos1.query.get(id)
+    todos1.complete = True
+    db.session.commit()
+    return "You've completed the task " + str(id)
+
+@app.route("/incomplete/<int:id>")
+def incomplete(id):
+    todos1 = todos1.query.get(id)
+    todos1.complete = False
+    db.session.commit()
+    return "You've incompleted the task " + str(id)
+
+@app.route("/delete/<int:id>")
+def delete(id):
+    todos1 = todos1.query.get(id)
+    db.session.delete(todos1)
+    db.session.commit()
+    return "You've deleted the task " + str(id)
 
 if __name__=='__main__':
     app.run(debug=True, host='0.0.0.0')
